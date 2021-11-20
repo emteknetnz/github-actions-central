5e3d4f058ad0189e994e6b2f1373c32343e398a7
Steve Boyd <emteknetnz@gmail.com>
2021-11-20 21:13:57 +1300

    .

--- .github/workflows/standards/scan.php
+++ .github/workflows/standards/scan.php
@@ -3,4 +3,23 @@
 include('modules.php');
 
 $modules = MODULES['regular']['silverstripe'];
-print_r($modules);
+$module = 'silverstripe-campaign-admin'
+
+// hit github api
+$token = $argv[1];
+
+function fetch($path, $token) {
+    $ch = curl_init();
+    curl_setopt($ch ,CURLOPT_URL, 'https://api.github.com' . $path);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Accept: application/vnd.github.v3+json',
+        "Authorization: $token"
+    ];
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $res = curl_exec($tuCurl);
+    curl_close($ch);
+    return $res;
+}
+
+
