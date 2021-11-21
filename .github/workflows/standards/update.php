087ef2c3470f3e08a0c27f20c181f0451b4648d9
Steve Boyd <emteknetnz@gmail.com>
2021-11-21 17:22:19 +1300

    .

--- .github/workflows/standards/update.php
+++ .github/workflows/standards/update.php
@@ -7,6 +7,38 @@
 // - for scanned things like .travis.yml, need to create a new github actions ci.yml workflow file
 // - which i'll copy in from templates, and add in composer_require_extra
 // - also delete .travis.yml while I'm at it
-function update() {
 
+// exists | missing | up-to-date | template-missing | <template-path>
+
+function handleTravisYml($val) {
+    if ($val == 'missing') {
+        return;
+    }
+    
+}
+
+function update($account, $repo, $scan) {
+
+    foreach ($scan as $filename => $val) {
+        switch($filename) {
+            case ('.travis.yml'):
+                handleTravisYml($val);
+                // delete file
+            default:
+                break;
+        }
+    }
+    // ci.yml + .travis.yml
+    $path = '.github/workflows/ci.yml';
+    if ($scan[$path] == 'missing') {
+        $contents = file_get_contents('templates/ci.yml');
+    } else {
+        $contents = file_get_contents($path);
+    }
+    $travisReqs = $scan['.travis.yml']['reqs'] ?? [];
+    if (!empty($travisReqs)) {
+        $ciReqs = $scan[$path]['reqs'] ?? [];
+        $reqs = array_merge($travisReqs, $ciReqs); // TODO ensure that travisreqs get overridden if same key exists
+        
+    }
 }
